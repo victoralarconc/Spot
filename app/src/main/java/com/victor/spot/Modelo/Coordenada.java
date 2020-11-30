6d1941f9b533d1108b4b4e7976361364cc1b0f5e
c-moraga <72470984+c-moraga@users.noreply.github.com>
2020-11-29 21:28:37 -0300

    Agregar Clases

---
+++ app/src/main/java/com/victor/spot/Modelo/Coordenada.java
@@ -0,0 +1,22 @@
+package com.victor.spot.Modelo;
+
+public class Coordenada {
+    Double latitud;
+    Double longitud;
+
+    public Double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(Double latitud) {
+        this.latitud = latitud;
+    }
+
+    public Double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Double longitud) {
+        this.longitud = longitud;
+    }
+}
